911d63e26f779a1e10ca74fc35d997c381b2fee5
Vitaly Chekryzhev <13hakta@gmail.com>
2015-02-23 02:45:12 +0500

    Fix DB on upgrade

--- _build/resolvers/resolve.tables.php
+++ _build/resolvers/resolve.tables.php
@@ -6,7 +6,7 @@ if ($object->xpdo) {
 
 	switch ($options[xPDOTransport::PACKAGE_ACTION]) {
 		case xPDOTransport::ACTION_INSTALL:
-			$modelPath = $modx->getOption('fileattach_core_path', null, $modx->getOption('core_path') . 'components/fileattach/') . 'model/';
+			$modelPath = $modx->getOption('fileattach.core_path', null, $modx->getOption('core_path') . 'components/fileattach/') . 'model/';
 			$modx->addPackage('fileattach', $modelPath);
 
 			$manager = $modx->getManager();
@@ -19,6 +19,26 @@ if ($object->xpdo) {
 			break;
 
 		case xPDOTransport::ACTION_UPGRADE:
+			// Upgrade DB scheme if there were older packages installed
+			$package = $modx->getObject('modTransportPackage', array(
+			    'package_name' => PKG_NAME,
+			    'version_major' => '1',
+			    'version_minor' => '0',
+			    'version_patch:<' => '2'));
+
+			if ($package) {
+        		    $oldLogLevel = $modx->getLogLevel();
+        		    $modx->setLogLevel(0);
+
+			    $modelPath = $modx->getOption('fileattach.core_path', null, $modx->getOption('core_path') . 'components/fileattach/') . 'model/';
+			    $modx->addPackage('fileattach', $modelPath);
+
+			    $manager = $modx->getManager();
+        		    $manager->addField('FileItem', 'rank');
+
+        		    $modx->setLogLevel($oldLogLevel);
+			}
+
 			break;
 
 		case xPDOTransport::ACTION_UNINSTALL:
--- core/components/fileattach/docs/readme.txt
+++ core/components/fileattach/docs/readme.txt
@@ -8,3 +8,6 @@ A basic Extra for MODx Revolution.
 
 Feel free to suggest ideas/improvements/bugs on GitHub:
 http://github.com/13hakta/FileAttach/issues
+
+Official docs:
+http://13hakta.ru/dev/modx/fileattach-en.html
