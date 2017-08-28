35205f1d1a090d9887bc649fb53a47fa45aaf159
Vitaly Chekryzhev <13hakta@gmail.com>
2017-08-28 15:09:43 +0500

    Remove unneeded element in build script

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -26,7 +26,6 @@ $sources = array(
 	'plugins' => $root . 'core/components/' . PKG_NAME_LOWER . '/elements/plugins/',
 	'lexicon' => $root . 'core/components/' . PKG_NAME_LOWER . '/lexicon/',
 	'docs' => $root . 'core/components/' . PKG_NAME_LOWER . '/docs/',
-	'pages' => $root . 'core/components/' . PKG_NAME_LOWER . '/elements/pages/',
 	'source_assets' => $root . 'assets/components/' . PKG_NAME_LOWER,
 	'source_core' => $root . 'core/components/' . PKG_NAME_LOWER,
 );
