a07a65deb4b0b43732c9239e3e89381184a005a4
Vitaly Chekryzhev <13hakta@gmail.com>
2018-12-11 18:18:46 +0500

    Make menu router-based
    Remove deprecated modAction handler.

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -144,19 +144,14 @@ if (defined('BUILD_POLICY_TEMPLATE_UPDATE')) {
 /* load menus */
 if (defined('BUILD_MENU_UPDATE')) {
 	$menus = include $sources['data'] . 'transport.menu.php';
+
 	$attributes = array(
 		xPDOTransport::PRESERVE_KEYS => true,
 		xPDOTransport::UPDATE_OBJECT => BUILD_MENU_UPDATE,
 		xPDOTransport::UNIQUE_KEY => 'text',
-		xPDOTransport::RELATED_OBJECTS => true,
-		xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array(
-			'Action' => array(
-				xPDOTransport::PRESERVE_KEYS => false,
-				xPDOTransport::UPDATE_OBJECT => BUILD_ACTION_UPDATE,
-				xPDOTransport::UNIQUE_KEY => array('namespace', 'controller'),
-			),
-		),
+		xPDOTransport::RELATED_OBJECTS => true
 	);
+
 	if (is_array($menus)) {
 		foreach ($menus as $menu) {
 			$vehicle = $builder->createVehicle($menu, $attributes);
